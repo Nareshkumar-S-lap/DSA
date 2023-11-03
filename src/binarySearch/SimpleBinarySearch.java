6502eb85544f542d3b64bd6c3150a9d38326bb2a
NareshKumar-S-lap <nareshlee720@gmail.com>
2023-11-03 22:08:26 +0530

    its a binary Search in array program

---
+++ src/binarySearch/SimpleBinarySearch.java
@@ -0,0 +1,26 @@
+package binarySearch;
+
+public class SimpleBinarySearch {
+    public static void main(String[] args) {
+        int[] nums = {23456, 7, 8, 53, 36, 3, 7, 84, 36, 67, 54, 78, 23};
+        System.out.println(binarysearch(nums, 78));
+    }
+
+    static int binarysearch(int[] nums, int target) {
+        int start = 0;
+        int end = nums.length-1;
+        while (start <= end) {
+            //find the middle element
+            int mid = start + (end - start) / 2;
+            if (target < nums[mid]) {
+                end = mid - 1;
+            } else if (target > nums[mid]) {
+                start = mid + 1;
+            } else {
+                return mid;
+            }
+        }
+        return -1;
+    }
+}
+
